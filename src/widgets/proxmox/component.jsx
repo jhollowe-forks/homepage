79eb4eb6ab67d8556d44b91328da693838b3aa29
John Hollowell <jhollowe@johnhollowell.com>
2022-11-23 04:44:19 +0000

    Add cluster multi-node support for proxmox widget

--- src/widgets/proxmox/component.jsx
+++ src/widgets/proxmox/component.jsx
@@ -11,7 +11,13 @@ function calcRunning(total, current) {
 export default function Component({ service }) {
   const { t } = useTranslation();
 
-  const { widget } = service;
+  const { widget, nodeNames } = service;
+
+  function doesNodeMatch(nodeName) {
+    // if not specified, use all nodes. Otherwise validate node is in list
+    return (!nodeNames || nodeNames === undefined ||
+      nodeNames.split(",").filter(name => name === nodeName).length != 0)
+  }
 
   const { data: clusterData, error: clusterError } = useWidgetAPI(widget, "cluster/resources");
 
@@ -30,24 +36,25 @@ export default function Component({ service }) {
     );
   }
 
-  const { data } = clusterData ;
-  const vms = data.filter(item => item.type === "qemu") || [];
-  const lxc = data.filter(item => item.type === "lxc") || [];
-  const nodes = data.filter(item => item.type === "node") || [];
+  const { data } = clusterData;
+  const nodes = data.filter(item => item.type === "node" && doesNodeMatch(item.node)) || [{ node: "", cpu: 0.0, mem: 0, maxmem: 0 }];
+  const vms = data.filter(item => item.type === "qemu" && doesNodeMatch(item.node)) || [];
+  const lxc = data.filter(item => item.type === "lxc" && doesNodeMatch(item.node)) || [];
 
   const runningVMs = vms.reduce(calcRunning, 0);
   const runningLXC = lxc.reduce(calcRunning, 0);
 
-  // TODO: support more than one node
-  // TODO: better handling of cluster with zero nodes
-  const node = nodes.length > 0 ? nodes[0] : { cpu: 0.0, mem: 0, maxmem: 0 };
+  // Sum together all the nodes' stats
+  const combinedNode = nodes.reduce((acc, curr) => { return { cpu: acc.cpu + curr.cpu, mem: acc.mem + curr.mem, maxmem: acc.maxmem + curr.maxmem } }, { cpu: 0.0, mem: 0, maxmem: 0 });
+  combinedNode.cpu /= nodes.length; // make an average rather than a sum
 
+  // console.log(nodes, combinedNode);
   return (
     <Container service={service}>
       <Block label="proxmox.vms" value={`${runningVMs} / ${vms.length}`} />
       <Block label="proxmox.lxc" value={`${runningLXC} / ${lxc.length}`} />
-      <Block label="proxmox.cpu" value={t("common.percent", { value: (node.cpu * 100) })} />
-      <Block label="proxmox.mem" value={t("common.percent", { value: ((node.mem / node.maxmem) * 100) })} />
+      <Block label="proxmox.cpu" value={t("common.percent", { value: (combinedNode.cpu * 100) })} />
+      <Block label="proxmox.mem" value={t("common.percent", { value: ((combinedNode.mem / combinedNode.maxmem) * 100) })} />
     </Container>
   );
 }
